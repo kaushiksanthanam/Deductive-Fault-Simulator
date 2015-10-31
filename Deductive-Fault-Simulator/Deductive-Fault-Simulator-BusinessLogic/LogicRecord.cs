2207a083ac63da8ed4613b32d75b2912be78d1f2
Kaushik Santhanam <doors2kaushik@gmail.com>
2015-10-31 17:12:30 -0400

    UI partial design

---
+++ Deductive-Fault-Simulator/Deductive-Fault-Simulator-BusinessLogic/LogicRecord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deductive_Fault_Simulator_BusinessLogic
+{
+    public class LogicRecord
+    {
+    }
+}
--- Deductive-Fault-Simulator/Deductive-Fault-Simulator/MainWindow.xaml.cs
+++ Deductive-Fault-Simulator/Deductive-Fault-Simulator/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,48 @@ namespace Deductive_Fault_Simulator
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Variables
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Event Handler for when browse for input 
+        /// file is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open a new File Dialog
+            OpenFileDialog ofd = new OpenFileDialog();
+        
+            if(ofd.ShowDialog().Value)
+            {
+                // Get the filePath, write it to text box
+                string filePath = ofd.FileName;
+                FileNameTextBox.Text = filePath;
+
+                // Read the filecontents and display on the
+                // file contents display box
+                string fileContents = File.ReadAllText(filePath);
+                FileContentsDisplayBox.Text = fileContents;
+            }
+        }
+
+        #endregion
     }
 }
